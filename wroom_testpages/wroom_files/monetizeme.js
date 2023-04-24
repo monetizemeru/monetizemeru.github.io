0f6f58c81f34163b1b964219f5a425134779b753
vladimir <vladimir@monetize-me.ru>
2023-04-24 19:24:36 +0400

    done

--- wroom_testpages/wroom_files/monetizeme.js
+++ wroom_testpages/wroom_files/monetizeme.js
@@ -41,17 +41,7 @@ var adriver_script = document.createElement('script');
 adriver_script.setAttribute('src','https://content.adriver.ru/AdRiverFPS.js');
 document.head.appendChild(adriver_script);
 
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();
-for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-ym(93344338, "init", {
-     clickmap:false,
-     trackLinks:true,
-     accurateTrackBounce:true
-});
 
 //LIBRARY CONNECT END
 
